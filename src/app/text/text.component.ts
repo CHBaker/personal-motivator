6fae92f662d9e757e3b304d204ff8dec87327ce1
Gabriel Babilonia <babiloniag@gmail.com>
2018-04-17 20:35:45 -0600

    Adding bits

--- src/app/text/text.component.ts
+++ src/app/text/text.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-text',
-  templateUrl: './text.component.html',
-  styleUrls: ['./text.component.css']
+    selector: "app-text",
+    templateUrl: "./text.component.html",
+    styleUrls: ["./text.component.css"]
 })
 export class TextComponent implements OnInit {
+    showModal = false;
+    constructor() {}
 
-  constructor() { }
+    ngOnInit() {}
 
-  ngOnInit() {
-  }
-
+    show() {
+        this.showModal = !this.showModal;
+        console.log("Open modal");
+    }
 }
