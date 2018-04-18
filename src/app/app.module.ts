9b7ec88b7a33d9b04abe644c0d52473b610f47fc
Zaeem Siddiqui <zaeem.siddiqui@chep.com>
2018-04-17 21:35:06 -0400

    Adding base bits for face component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { VoiceComponent } from './voice/voice.component';
+import { FaceComponent } from './face/face.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent, VoiceComponent
+    AppComponent, VoiceComponent, FaceComponent
   ],
   imports: [
     BrowserModule
